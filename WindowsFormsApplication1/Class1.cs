ed11d3090bf7665c8cbdfaf4a880912cfb378371
Robert Schönherr <rbs@informatik.tu-chemnitz.de>
2014-02-16 15:45:28 +0100

    first commit, seems to work

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Key
+    {
+        private string _CDKey = "";
+        private string _HexData = "";
+        private byte[] _ByteData = new byte[15];
+        public bool SkipCleaning = false;
+        public bool SkipChecks = false;
+        public bool UseDivider = true;
+        public char CDKeyDivider = '-';
+        public char HexDataDivider = ',';
+
+        public byte[] ByteData
+        {
+            get { return _ByteData; }
+        }
+
+        public string CDKey
+        {
+            get { return _CDKey; }
+            set
+            {
+                string vaildChars = "0123456789ABCDEFGHJKLMNPRSTVWXYZ";
+                bool vaildFormat = true;
+                if (!SkipCleaning)
+                {
+                    value = value.ToUpper();
+                    value = value.Replace("-", "");
+                    value = value.Replace(",", "");
+                    value = value.Replace(" ", "");
+                    value = value.Replace("O", "0");
+                    value = value.Replace("I", "1");
+                }
+                if (!SkipChecks)
+                {
+                    if (value.Length != 24)
+                        vaildFormat = false;
+                    else
+                    {
+                        for (int i = 0; i < value.Length && vaildFormat; i++)
+                        {
+                            char currentChar = value[i];
+                            bool vaildChar = false;
+                            for (int ii = 0; ii < vaildChars.Length && !vaildChar; ii++) if (currentChar == vaildChars[ii]) vaildChar = true;
+                            if (!vaildChar) vaildFormat = false;
+                        }
+                    }
+                    
+                }
+                if (vaildFormat)
+                {
+                    _ByteData = new byte[15];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        UInt64 BitwiseResult = 0;
+                        for (int ii = 0; ii < 8; ii++)
+                        {
+                            BitwiseResult |= (UInt64)vaildChars.IndexOf(value[i * 8 + ii]) << (ii * 5);
+                        }
+                        for (int ii = 0; ii < 5; ii++)
+                        {
+                            _ByteData[i * 5 + 5 - 1 - ii] += Convert.ToByte(BitwiseResult & 0xff);
+                            BitwiseResult >>= 8;
+                        }
+                    }
+                    _HexData = BitConverter.ToString(_ByteData);
+                    if (UseDivider)
+                    {
+                        if (HexDataDivider != '-') _HexData = _HexData.Replace('-', HexDataDivider);
+                        value = value.Insert(4, new string(CDKeyDivider, 1));
+                        value = value.Insert(10, new string(CDKeyDivider, 1));
+                        value = value.Insert(16, new string(CDKeyDivider, 1));
+                        value = value.Insert(22, new string(CDKeyDivider, 1));
+                    }
+                    else
+                    {
+                        _HexData = _HexData.Replace('-', '\0');
+                    }
+                    _CDKey = value;
+                }
+                else
+                    _HexData = "";
+            }
+        }
+
+        
+
+        public string HexData
+        {
+            get { return _HexData; }
+            set
+            {
+                string vaildHexChars = "0123456789ABCDEF";
+                string vaildKeyChars = "0123456789ABCDEFGHJKLMNPRSTVWXYZ";
+                bool vaildFormat = true;
+                if (!SkipCleaning)
+                {
+                    value = value.ToUpper();
+                    value = value.Replace("-", "");
+                    value = value.Replace(",", "");
+                    value = value.Replace(" ", "");
+                }
+                if (!SkipChecks)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        char currentChar = value[i];
+                        bool vaildChar = false;
+                        for (int ii = 0; ii < vaildHexChars.Length && !vaildChar; ii++) if (currentChar == vaildHexChars[ii]) vaildChar = true;
+                        if (!vaildChar) vaildFormat = false;
+                    }
+                }
+                if (vaildFormat)
+                {
+                    _ByteData = Enumerable.Range(0, value.Length).Where(x => x % 2 == 0).Select(x => Convert.ToByte(value.Substring(x, 2), 16)).ToArray();
+                    _CDKey = "";
+                    for (int i = 0; i < 3; i++)
+                    {
+                        UInt64 BitwiseResult = 0;
+                        for (int ii = 0; ii != 5; ii++)
+                        {
+                            BitwiseResult = _ByteData[i * 5 + ii] | (BitwiseResult << 8);
+                        }
+                        for (int ii = 0; ii != 8; ii++)
+                        {
+                            _CDKey += vaildKeyChars[(int)(BitwiseResult & 0x1f)];
+                            BitwiseResult >>= 5;
+                        }
+                    }
+                    _HexData = BitConverter.ToString(_ByteData);
+                    if (UseDivider)
+                    {
+                        if (HexDataDivider != '-') _HexData = _HexData.Replace('-', HexDataDivider);
+                        _CDKey = _CDKey.Insert(4, new string(CDKeyDivider, 1));
+                        _CDKey = _CDKey.Insert(10, new string(CDKeyDivider, 1));
+                        _CDKey = _CDKey.Insert(16, new string(CDKeyDivider, 1));
+                        _CDKey = _CDKey.Insert(22, new string(CDKeyDivider, 1));
+                    }
+                    else
+                    {
+                        _HexData = _HexData.Replace('-', '\0');
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Key key = new Key();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            key.CDKey = textBox1.Text;
+            textBox2.Text = key.HexData;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey localMachine64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey rkey = localMachine64.OpenSubKey(@"SOFTWARE\Wow6432Node\bohemia interactive studio\arma 2", true);
+
+            key.CDKey = textBox1.Text;
+            textBox2.Text = key.HexData;
+
+            if (key.HexData != "")
+            {
+                rkey.SetValue("key", key.ByteData, RegistryValueKind.Binary);
+                MessageBox.Show("OK");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Serial format.");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey localMachine64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
+            RegistryKey rkey = localMachine64.OpenSubKey(@"SOFTWARE\Wow6432Node\bohemia interactive studio\arma 2 oa", true);
+
+            key.CDKey = textBox1.Text;
+            textBox2.Text = key.HexData;
+            if (key.HexData != "")
+            {
+                rkey.SetValue("key", key.ByteData, RegistryValueKind.Binary);
+                MessageBox.Show("OK");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Serial format.");
+            }
+        }
+
+    }
+}
